1c6007eb5ba064c54056decd1e03e0ee6ce59d4c
Mateusz Wojcik <mateuszwojcik19@gmail.com>
2020-01-12 16:14:01 +0100

    Init

---
+++ HashCode2020PreparationsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashCode2020PreparationsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
